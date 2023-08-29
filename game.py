71b846d56e2eb0960a1483dab27d2896658cc178
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-29 07:06:18 -0500

    create prompt to ask color bet

--- game.py
+++ game.py
@@ -4,15 +4,29 @@ t1 = Turtle()
 t2 = Turtle()
 t3 = Turtle()
 t4 = Turtle()
+
 screen = Screen()
+# Set up the dimensions of the screen
+screen.setup(500,400)
 
+# Create a user prompt
+user_bet = screen.textinput("Make your bet", "Which turtle will win the race? Enter a color: ")
 
+print(user_bet)
 
+t1.color('brown')
+t2.color('blue')
+t3.color('green')
+t4.color('black')
+
+t1.shape("turtle")
+t1.penup()
+t1.goto(-235, 0)
 
 screen.listen()
 
 # Event listener than takes in 2 arguments: key and function
-screen.onkey(key= "w", fun=)
+# screen.onkey(key= "w", fun=)
 
 
 

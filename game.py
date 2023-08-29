d6bdeca7b4e1ee89390537c568fdef5b97e1ba7f
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-29 06:53:08 -0500

    created 4 turtle objects as instances

--- game.py
+++ game.py
@@ -1,6 +1,20 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
+t1 = Turtle()
+t2 = Turtle()
+t3 = Turtle()
+t4 = Turtle()
 screen = Screen()
 
+
+
+
 screen.listen()
+
+# Event listener than takes in 2 arguments: key and function
+screen.onkey(key= "w", fun=)
+
+
+
+
+screen.exitonclick() 
